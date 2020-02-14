919c3a764eeabd55778e351343f6cfa15e9e0051
JiaLZhong <60520619+JiaLZhong@users.noreply.github.com>
2020-02-14 14:15:33 -0500

    Update TestGacha.cpp

--- TestGacha.cpp
+++ TestGacha.cpp
@@ -9,54 +9,59 @@ Program Purpose: Work as a Gacha
 using namespace std;
 
 int main()
-{
    
-   int LR,UR,SSR,SR,R,Balance,Cost,Value,Time;
-   LR = 100;//(1% Change of Getting a LR Monster Card.)  
+   int LR,UR,SSR,SR,R,Balance,Cost,Value,Summon_num;
+   LR = 100;//(1% Change of Getting a LR Monster Card.) 
    UR = 95; //(5% Change of Getting a UR Monster Card.) 
    SSR =85; //(10% Change of Getting a SSR Monster Card.)
    SR = 67; //(18% Change of Getting a SR Monster Card.)
    R = 42;//(25% Change of Getting a R Monster Card.)            
-   //N=1 (59% Change of Getting a Normal Monster Card.) ~Not needed because of else
-   Time = 1; 
+ //int N = 1 (59% Change of Getting a Normal Monster Card.) ~ Not needed because of else
+   Summon_num = 1;
    
    cout <<"Welcome to Monster Card Gacha" << endl;
-   
-   //Making sure the random number algorithm is mixed 
+
+   //Allow the random number algorithm  to mixed
    srand(time(0));
    
-   for(Time = 0;Time <=10;Time++)
+   for(int i = 1;i <=10;i++)
    {
-      //Getting a random number form 1 to 100.
+      
       Value = (rand () % 100) + 1;
+      
+      //cout << Value <<endl; ~ use to check if the num is random.
       
       if (Value >= LR)
       {
-         cout << "You got a Legendary-Rare Monster Card" << endl;
+         cout << Summon_num << ":You got a Legendary-Rare Monster Card" << endl;
+         Summon_num ++;
       }
       else if (Value >= UR)
       {
-         cout << "You got a Ultra-Rare Monster Card" << endl;
+         cout << Summon_num << ":You got a Ultra-Rare Monster Card" << endl;
+         Summon_num ++;
       }
       else if (Value >= SSR)
       {
-         cout << "You got a Super-Super-Rare Monster Card" << endl;
+         cout << Summon_num << ":You got a Super-Super-Rare Monster Card" << endl;
+         Summon_num ++;
       }
       else if (Value >= SR)
       {
-         cout << "You got a Super-Rare Monster Card" << endl;
+         cout << Summon_num << ":You got a Super-Rare Monster Card" << endl;
+         Summon_num ++;
       }
       else if (Value >= R)
       {
-         cout << "You got a Rare Monster Card" << endl;
+         cout << Summon_num << ":You got a Rare Monster Card" << endl;
+         Summon_num ++;
       }
       else
       {
-         cout<< "You got a Normal Moster Card" << endl;
+         cout<< Summon_num << ":You got a Normal Moster Card" << endl;
+         Summon_num ++;
       }   
    }   
-   
    system("pause");
-    
    return 0;
 }
